cf478a7da5567c555d04f7f11b0171ef253ad97c
james-berkheimer <james.berkheimer@gmail.com>
2023-11-11 16:19:03 -0800

    Pushing for moving

--- src/make_database/main.py
+++ src/make_database/main.py
@@ -1,4 +1,15 @@
+import redis
+from redis.cluster import RedisCluster
 
 
 def main():
-    print("Hello")
+    r = redis.Redis(host="localhost", port=6379, decode_responses=True)
+    r.set("foo", "bar")
+    print(r.get("foo"))
+    r.hset(
+        "user-session:123",
+        mapping={"name": "John", "surname": "Smith", "company": "Redis", "age": 29},
+    )
+    print(r.hgetall("user-session:123"))
+    # rc = RedisCluster(host="127.0.0.1", port=6379)
+    # print(rc.get_nodes())
