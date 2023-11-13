9ee8a8145b4ca5fb5f16c822130e225f15641fc0
james-berkheimer <james.berkheimer@gmail.com>
2023-11-13 14:29:28 -0800

    moving

--- src/make_database/main.py
+++ src/make_database/main.py
@@ -3,13 +3,7 @@ from redis.cluster import RedisCluster
 
 
 def main():
-    r = redis.Redis(host="localhost", port=6379, decode_responses=True)
-    r.set("foo", "bar")
-    print(r.get("foo"))
-    r.hset(
-        "user-session:123",
-        mapping={"name": "John", "surname": "Smith", "company": "Redis", "age": 29},
-    )
-    print(r.hgetall("user-session:123"))
-    # rc = RedisCluster(host="127.0.0.1", port=6379)
-    # print(rc.get_nodes())
+    # r = redis.Redis(host="localhost", port=6379, decode_responses=True)
+    r = redis.Redis(decode_responses=True)
+    r.mset({"Croatia": "Zagreb", "Bahamas": "Nassau"})
+    print(r.get("Bahamas"))
